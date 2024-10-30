05f9d933cb2c6a3d160f683676490e0e39c2281a
jaehyeon4406 <jaehyeon4406@gmail.com>
2024-10-30 12:18:20 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/사칙연산 함수/quadratic-operations-function.c
+++ 241030/사칙연산 함수/quadratic-operations-function.c
@@ -5,7 +5,12 @@ int main() {
     int a, c;
     char o;
     scanf("%d %c %d", &a, &o, &c);
-    printf("%d %c %d = %d", a, o, c, get_math(a, o, c));
+    if (o == '+' || o == '-' || o == '/' || o == '*') {
+        printf("%d %c %d = %d", a, o, c, get_math(a, o, c));
+    }
+    else {
+        printf("False");
+    }
     return 0;
 }
 
